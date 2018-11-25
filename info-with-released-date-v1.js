f8ef2ad719704b46a6489eafe469c2e93ccbe3af
Nekopoi <44767728+nkpoi@users.noreply.github.com>
2018-11-25 22:06:25 +0800

    Update info-with-released-date-v1.js

--- info-with-released-date-v1.js
+++ info-with-released-date-v1.js
@@ -26,7 +26,7 @@ function post_latest(M) {
       J = F.substring(5, 7),
       K = F.substring(8, 10),
       x = new Array;
-    x[1] = "Jan, x[2] = "Feb", x[3] = "Mar", x[4] = "Apr", x[5] = "May", x[6] = "Jun", x[7] = "Jul", x[8] = "Agu", x[9] = "Sept", x[10] = "Okt", x[11] = "Nov", x[12] = "Des",
+    x[1] = "Jan", x[2] = "Feb", x[3] = "Mar", x[4] = "Apr", x[5] = "May", x[6] = "Jun", x[7] = "Jul", x[8] = "Agu", x[9] = "Sept", x[10] = "Okt", x[11] = "Nov", x[12] = "Des",
       document.write('<li class="last-post-item">'), document.write('<div class="post_item"><a href="' + j + '" target ="_top">' + E + "</a></div>"), 1 == post_rilis && document.write('<span class="post-update">' + K + " " + x[parseInt(J, 10)] + " " + H + "</span>");
     var G = "",
       D = 0;
