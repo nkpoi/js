9200aa4008c8528125644d4ddaff0ba418f4288b
Nekopoi <44767728+nkpoi@users.noreply.github.com>
2020-02-07 18:14:17 +0800

    Add files via upload

---
+++ streaming.js
@@ -0,0 +1,14 @@
+<script type='text/javascript'>//<![CDATA[
+// Light Button
+$(document).ready(function(){$("#lightsoff").css("height", $(document).height()).hide();$(".lightSwitcher").click(function(){$("#lightsoff").toggle();if ($("#lightsoff").is(":hidden"))$(this).html("Turn off the Lights").removeClass("turnedOff");else $(this).html("Turn on the Lights").addClass("turnedOff");});});
+// Streaming
+$(document).ready(function(){
+$('b[name="coverxz"]').before($("#iamgex-cover").html());$("#iamgex-cover").html("");
+ (function () {
+  var a = $(".frame-videos");
+  $(".ganti-eps").click(function () {
+   a.html('<div class="--responsive"><iframe src="' + mov[parseInt($(this).data("id"))] + '" allowfullscreen="true" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" width="100%" height="360"  ></iframe></div>')
+  })
+ }());
+});
+//]]></script>
